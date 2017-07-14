4708db57a2d0b33e46f88a906210e4f3ea426d8c
SaSol <mwielgus@outlook.com>
2017-07-15 00:14:22 +0200

    Customizable docker version via SHATHEL_SOLUTION_DOCKER_VERSION

--- virtualbox/Vagrantfile-image.sh
+++ virtualbox/Vagrantfile-image.sh
@@ -26,7 +26,7 @@ echo "sudo apt-cache madison docker ..."
 sudo apt-cache madison docker-ce
 
 echo "sudo apt-get install docker ..."
-sudo apt-get install docker-ce=17.03.1~ce-0~ubuntu-xenial -y
+sudo apt-get install docker-ce=${INSTALL_DOCKER_VERSION}~ce-0~ubuntu-xenial -y
 
 echo "sudo usermod -a -G ..."
 sudo usermod -a -G docker ubuntu
