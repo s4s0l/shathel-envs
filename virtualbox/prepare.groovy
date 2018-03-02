50ff98d36258b24affd168270d474893ef5c0fb1
Marcin Wielgus <mwielgus@outlook.com>
2018-03-02 23:06:37 +0100

    vbox env has larger VDI discs

--- virtualbox/prepare.groovy
+++ virtualbox/prepare.groovy
@@ -1,5 +1,7 @@
 def imageName = env['SHATHEL_ENVPACKAGE_IMAGE_NAME']
 
+processor.run("vagrant", "plugin install vagrant-disksize", "eee", env)
+
 if (!processor.run("vagrant", "box list", "./Vagrantfile-image", env).output.contains(imageName)) {
     processor.run("vagrant", "up", "./Vagrantfile-image", env)
     processor.run("vagrant", "package --output ${env['SHATHEL_ENVPACKAGE_TMP_DIR']}/${imageName}.box", "./Vagrantfile-image", env)
